92c11bbcbe05b553c89760c410ad6a79fd542856
mpabbathi <149748818+mpabbathi@users.noreply.github.com>
2025-11-05 00:15:02 -0800

    Update minu_analysis.R

--- scripts/minu_analysis.R
+++ scripts/minu_analysis.R
@@ -121,3 +121,59 @@ bind_rows(
 
 # Instead of selecting for just 10 proteins, we selected for 20. This improved the sensitivity by 6%, 
 # specificity by 13%, accuracy by 9%, and AUC by 4%.
+
+# QUESTION 3 PART 3 (fuzzy intersection)
+proteins_s1_fuzzy <- ttests_out %>%
+  slice_min(p.adj, n = 10) %>%
+  pull(protein)
+
+proteins_s2_fuzzy <- rf_out$importance %>% 
+  as_tibble() %>%
+  mutate(protein = rownames(rf_out$importance)) %>%
+  slice_max(MeanDecreaseGini, n = 10) %>%
+  pull(protein)
+
+proteins_sstar_fuzzy <- union(proteins_s1_fuzzy, proteins_s2_fuzzy)
+
+biomarker_sstar_fuzzy <- biomarker_clean %>%
+  select(group, any_of(proteins_sstar_fuzzy)) %>%
+  mutate(class = factor(group == 'ASD', levels = c(FALSE, TRUE))) %>% 
+  select(-group)
+
+# partition into training and test set
+set.seed(101422)
+biomarker_split_fuzzy <- biomarker_sstar_fuzzy %>%
+  initial_split(prop = 0.8)
+
+# fit logistic regression model to training set
+fit_fuzzy <- glm(class ~ ., 
+           data = training(biomarker_split_fuzzy), 
+           family = 'binomial')
+
+# predict on test data
+predictions_fuzzy <- testing(biomarker_split_fuzzy) %>%
+  mutate(pred = predict(fit_fuzzy, ., type = "response"),
+         pred_class = factor(ifelse(pred > 0.5, TRUE, FALSE),
+                             levels = c(FALSE, TRUE)))
+
+# evaluate metrics
+class_metrics <- metric_set(sensitivity, specificity, accuracy)
+
+bind_rows(
+  class_metrics(predictions_fuzzy,
+                truth = class,
+                estimate = pred_class,
+                event_level = "second"),
+  roc_auc(predictions_fuzzy,
+          truth = class,
+          pred,
+          event_level = "second")
+)
+
+# sensitivity: 0.562
+# specificity: 0.733
+# accuracy: 0.645
+# AUC: 0.779
+
+# Instead of doing a hard intersection, we looked at a fuzzy intersection of the proteins. This decreased the
+#  sensitivity by 20%, specificity by 7%, accuracy by 10%, and AUC by 12%.
